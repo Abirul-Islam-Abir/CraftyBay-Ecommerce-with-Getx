27eb1135d0b34b6e002841d6b723a91a5a43666b
Abirul Islam Abir <abir80503@gmail.com>
2023-10-09 08:54:48 +0600

    Modules added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
+
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-import 'modules/landing page/landing_page.dart'; 
-Future<void> main() async {
-  await appInitialization();
-  runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const MyApp(), // Wrap your app
-    ),
-  );
-}
+import 'modules/landing page/landing_page.dart';
 
-Future appInitialization() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+
+    runApp(const MyApp());
+
+  /*runApp(DevicePreview(
+      enabled: !kReleaseMode, builder: (context) => const MyApp()));*/
 }
+
