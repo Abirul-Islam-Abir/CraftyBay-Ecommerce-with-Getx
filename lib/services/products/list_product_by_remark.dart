57de656b91fc0f5a4fd5af0362aa3d39bd3728e2
Abirul Islam Abir <abir80503@gmail.com>
2023-10-16 15:22:21 +0600

    Update list_product_by_remark.dart

--- lib/services/products/list_product_by_remark.dart
+++ lib/services/products/list_product_by_remark.dart
@@ -1,1 +1,24 @@
-import 'dart:convert';import 'package:http/http.dart' as http;Future fetchListProductByRemarkRequest(statusUrl) async {  try {    final url = Uri.parse(statusUrl);    final http.Response response = await http.get(url);    if (response.statusCode == 200) {      final responseBody = jsonDecode(response.body);      if (responseBody['msg'] == 'success') {        final List<Map<String, dynamic>> data =            List<Map<String, dynamic>>.from(responseBody['data']);        return data;      } else {        throw Exception('Error has occurred: ${responseBody['msg']}');      }    } else {      throw Exception('Error has occurred: ${response.statusCode}');    }  } catch (e) {    throw Exception('Error has occurred: $e');  }}
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future fetchListProductByRemarkRequest(statusUrl) async {
+  try {
+    final url = Uri.parse(statusUrl);
+    final http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      final responseBody = jsonDecode(response.body);
+      if (responseBody['msg'] == 'success') {
+        final List<Map<String, dynamic>> data =
+            List<Map<String, dynamic>>.from(responseBody['data']);
+        return data;
+      } else {
+        throw Exception('Error has occurred: ${responseBody['msg']}');
+      }
+    } else {
+      throw Exception('Error has occurred: ${response.statusCode}');
+    }
+  } catch (e) {
+    throw Exception('Error has occurred: $e');
+  }
+}
