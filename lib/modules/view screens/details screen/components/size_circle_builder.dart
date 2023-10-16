4bf0068e32b03c994692ff33ad86fe0266d3bd18
Abirul Islam Abir <abir80503@gmail.com>
2023-10-16 14:53:39 +0600

    Update size_circle_builder.dart

--- lib/modules/view screens/details screen/components/size_circle_builder.dart
+++ lib/modules/view screens/details screen/components/size_circle_builder.dart
@@ -1,1 +1,39 @@
-import 'package:crafty_bay_ecommerce/modules/view%20screens/details%20screen/components/size_circle.dart';import '../../../../utils/export.dart';import '../../../widgets/normal_text.dart';import '../controller/produts_details_screen_controller.dart';class SizeCircleBuilder extends GetView<ProductDetailsScreenController> {  const SizeCircleBuilder(this.list, {super.key});  final List list;  @override  Widget build(BuildContext context) {    return Column(      crossAxisAlignment: CrossAxisAlignment.start,      children: [        const Padding(          padding: EdgeInsets.symmetric(horizontal: kTooSmallSize),          child: NormalText('Size'),        ),        Padding(          padding: const EdgeInsets.symmetric(horizontal: kTooSmallSize),          child: Row(              children: List.generate(            list.length,            (index) => SizeCircle(              selectedIndex: controller.sizeIndex,              index: index,              text: list[index],              onTap: () {                controller.sizeSelect(index);              },            ),          )),        ),      ],    );  }}
+import 'package:crafty_bay_ecommerce/modules/view%20screens/details%20screen/components/size_circle.dart';
+
+import '../../../../utils/export.dart';
+import '../../../widgets/normal_text.dart';
+import '../controller/produts_details_screen_controller.dart';
+
+class SizeCircleBuilder extends GetView<ProductDetailsScreenController> {
+  const SizeCircleBuilder(this.list, {super.key});
+
+  final List list;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: kTooSmallSize),
+          child: NormalText('Size'),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kTooSmallSize),
+          child: Row(
+              children: List.generate(
+            list.length,
+            (index) => SizeCircle(
+              selectedIndex: controller.sizeIndex,
+              index: index,
+              text: list[index],
+              onTap: () {
+                controller.sizeSelect(index);
+              },
+            ),
+          )),
+        ),
+      ],
+    );
+  }
+}
