437481591025c6541cfd88cb515323eab74f89a8
Abirul Islam Abir <abir80503@gmail.com>
2023-10-16 15:50:30 +0600

    Update app_colors.dart

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,1 +1,19 @@
-import 'package:flutter/material.dart';class AppColor {  static const kPrimaryColor = Colors.teal;  static const kTextColor = Colors.black54;  static const kWhiteColor = Colors.white;  static const kBlueColor = Colors.blue;  static const kRedColor = Colors.red;  static const kOrangeColor = Colors.orangeAccent;  static const kPurpleColor = Colors.deepPurple;  static const kBlackColor = Colors.black;  static const kGreyColor = Colors.grey;  static const kTransparentColor = Colors.transparent;  static const kGreenColor = Colors.lightGreen;  static const shimmerBaseColor = Color(0x37171721);  static const shimmerHighLightColor = Color(0x67FFFFFF);  AppColor._();}
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const kPrimaryColor = Colors.teal;
+  static const kTextColor = Colors.black54;
+  static const kWhiteColor = Colors.white;
+  static const kBlueColor = Colors.blue;
+  static const kRedColor = Colors.red;
+  static const kOrangeColor = Colors.orangeAccent;
+  static const kPurpleColor = Colors.deepPurple;
+  static const kBlackColor = Colors.black;
+  static const kGreyColor = Colors.grey;
+  static const kTransparentColor = Colors.transparent;
+  static const kGreenColor = Colors.lightGreen;
+  static const shimmerBaseColor = Color(0x37171721);
+  static const shimmerHighLightColor = Color(0x67FFFFFF);
+
+  AppColor._();
+}
