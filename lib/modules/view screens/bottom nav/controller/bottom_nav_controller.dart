32217b4ab713c856c53db6538fec52e30ad75713
Abirul Islam Abir <abir80503@gmail.com>
2023-10-16 14:52:05 +0600

    Update bottom_nav_controller.dart

--- lib/modules/view screens/bottom nav/controller/bottom_nav_controller.dart
+++ lib/modules/view screens/bottom nav/controller/bottom_nav_controller.dart
@@ -1,1 +1,19 @@
-import 'package:get/get.dart';class BottomNavController extends GetxController {  int navIndex = 0;  void navSelectedIndex(index) {    navIndex = index;    update();  }  void goToHome() {    navIndex = 0;    update();  }  void goToCategoryScreen() {    navIndex = 1;    update();  }}
+import 'package:get/get.dart';
+
+class BottomNavController extends GetxController {
+  int navIndex = 0;
+  void navSelectedIndex(index) {
+    navIndex = index;
+    update();
+  }
+
+  void goToHome() {
+    navIndex = 0;
+    update();
+  }
+
+  void goToCategoryScreen() {
+    navIndex = 1;
+    update();
+  }
+}
