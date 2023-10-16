68ea7218d7a7c558032f0de22fa4eb346542e168
Abirul Islam Abir <abir80503@gmail.com>
2023-10-16 14:59:27 +0600

    Update categories_button.dart

--- lib/modules/view screens/home screen/component/categories_button.dart
+++ lib/modules/view screens/home screen/component/categories_button.dart
@@ -1,1 +1,45 @@
-import 'package:crafty_bay_ecommerce/modules/view%20screens/home%20screen/controller/home_screen_controller.dart';import 'package:crafty_bay_ecommerce/modules/widgets/category_button_shimmer.dart';import '../../../../theme/app_colors.dart';import '../../../../theme/app_text_style.dart';import '../../../../utils/export.dart';class CategoriesButton extends StatelessWidget {  const CategoriesButton({    super.key,    required this.categoryName,    required this.onTap,  });  final String categoryName;  final GestureTapCallback onTap;  @override  Widget build(BuildContext context) {    return GetBuilder<HomeScreenController>(        builder: (controller) => controller.isLoading            ? CategoryButtonShimmer(categoryName: categoryName)            : Container(                width: Get.width,                padding: const EdgeInsets.symmetric(                    vertical: kTooSmallSize, horizontal: kTextSize),                child: Row(                  mainAxisAlignment: MainAxisAlignment.spaceBetween,                  children: [                    Text(                      categoryName,                      style: titleMedium(fontWeight: FontWeight.bold),                    ),                    InkWell(                      onTap: onTap,                      child: Text(                        'See All',                        style: titleTooSmall(color: AppColor.kPrimaryColor),                      ),                    ),                  ],                ),              ));  }}
+import 'package:crafty_bay_ecommerce/modules/view%20screens/home%20screen/controller/home_screen_controller.dart';
+import 'package:crafty_bay_ecommerce/modules/widgets/category_button_shimmer.dart';
+
+import '../../../../theme/app_colors.dart';
+import '../../../../theme/app_text_style.dart';
+import '../../../../utils/export.dart';
+
+class CategoriesButton extends StatelessWidget {
+  const CategoriesButton({
+    super.key,
+    required this.categoryName,
+    required this.onTap,
+  });
+
+  final String categoryName;
+  final GestureTapCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<HomeScreenController>(
+        builder: (controller) => controller.isLoading
+            ? CategoryButtonShimmer(categoryName: categoryName)
+            : Container(
+                width: Get.width,
+                padding: const EdgeInsets.symmetric(
+                    vertical: kTooSmallSize, horizontal: kTextSize),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      categoryName,
+                      style: titleMedium(fontWeight: FontWeight.bold),
+                    ),
+                    InkWell(
+                      onTap: onTap,
+                      child: Text(
+                        'See All',
+                        style: titleTooSmall(color: AppColor.kPrimaryColor),
+                      ),
+                    ),
+                  ],
+                ),
+              ));
+  }
+}
