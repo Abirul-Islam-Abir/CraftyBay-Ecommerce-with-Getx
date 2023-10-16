8bfef2906557a726d2fb5a16bf4589ec688ba97d
Abirul Islam Abir <abir80503@gmail.com>
2023-10-16 15:04:56 +0600

    Update headline_text.dart

--- lib/modules/widgets/headline_text.dart
+++ lib/modules/widgets/headline_text.dart
@@ -1,1 +1,30 @@
-import 'package:flutter/material.dart';import 'package:google_fonts/google_fonts.dart';import '../../theme/app_colors.dart';import '../../utils/app_size.dart';class HeadlineText extends StatelessWidget {  const HeadlineText({    super.key,    required this.text,    this.alignment = Alignment.topLeft,  });  final String text;  final Alignment alignment;  @override  Widget build(BuildContext context) {    return Align(      alignment: alignment,      child: Text(        text,        style: GoogleFonts.lilitaOne(          fontSize: kBigSize,          fontStyle: FontStyle.italic,          color: AppColor.kPrimaryColor,        ),      ),    );  }}
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../theme/app_colors.dart';
+import '../../utils/app_size.dart';
+
+class HeadlineText extends StatelessWidget {
+  const HeadlineText({
+    super.key,
+    required this.text,
+    this.alignment = Alignment.topLeft,
+  });
+
+  final String text;
+  final Alignment alignment;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: alignment,
+      child: Text(
+        text,
+        style: GoogleFonts.lilitaOne(
+          fontSize: kBigSize,
+          fontStyle: FontStyle.italic,
+          color: AppColor.kPrimaryColor,
+        ),
+      ),
+    );
+  }
+}
