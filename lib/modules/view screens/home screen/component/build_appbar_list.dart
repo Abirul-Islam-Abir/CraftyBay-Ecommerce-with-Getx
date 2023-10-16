205944488aad13a8c439b42e250db15781ab509b
Abirul Islam Abir <abir80503@gmail.com>
2023-10-16 14:58:59 +0600

    Update build_appbar_list.dart

--- lib/modules/view screens/home screen/component/build_appbar_list.dart
+++ lib/modules/view screens/home screen/component/build_appbar_list.dart
@@ -1,1 +1,11 @@
-import '../../../../utils/export.dart';import '../../../widgets/normal_text.dart';AppBar buildAppBarList(text) => AppBar(      title: Row(        crossAxisAlignment: CrossAxisAlignment.start,        children: [          NormalText(text),        ],      ),    );
+import '../../../../utils/export.dart';
+import '../../../widgets/normal_text.dart';
+
+AppBar buildAppBarList(text) => AppBar(
+      title: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          NormalText(text),
+        ],
+      ),
+    );
