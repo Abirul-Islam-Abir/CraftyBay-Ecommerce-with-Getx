f9d89ad53de127089e920a3123fccf6d5f0525fe
Abirul Islam Abir <abir80503@gmail.com>
2023-10-16 15:05:01 +0600

    Update long_line_text.dart

--- lib/modules/widgets/long_line_text.dart
+++ lib/modules/widgets/long_line_text.dart
@@ -1,1 +1,43 @@
-import 'package:flutter/material.dart';import 'package:google_fonts/google_fonts.dart';import '../../theme/app_colors.dart';import '../../utils/app_size.dart';class LongLineText extends StatelessWidget {  const LongLineText({    super.key,    required this.onTap,    required this.text,    required this.btnTxt,  });  final Function() onTap;  final String text;  final String btnTxt;  @override  Widget build(BuildContext context) {    return InkWell(      onTap: onTap,      child: RichText(        text: TextSpan(          children: [            TextSpan(              text: text,              style: GoogleFonts.fjallaOne(                  color: AppColor.kTextColor, fontSize: kTextSize),            ),            TextSpan(              text: btnTxt,              style: GoogleFonts.fjallaOne(                  color: AppColor.kPrimaryColor,                  fontSize: kDefaultSize,                  fontWeight: FontWeight.bold,                  decoration: TextDecoration.underline),            ),          ],        ),      ),    );  }}
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../theme/app_colors.dart';
+import '../../utils/app_size.dart';
+
+class LongLineText extends StatelessWidget {
+  const LongLineText({
+    super.key,
+    required this.onTap,
+    required this.text,
+    required this.btnTxt,
+  });
+
+  final Function() onTap;
+  final String text;
+  final String btnTxt;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: text,
+              style: GoogleFonts.fjallaOne(
+                  color: AppColor.kTextColor, fontSize: kTextSize),
+            ),
+            TextSpan(
+              text: btnTxt,
+              style: GoogleFonts.fjallaOne(
+                  color: AppColor.kPrimaryColor,
+                  fontSize: kDefaultSize,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
