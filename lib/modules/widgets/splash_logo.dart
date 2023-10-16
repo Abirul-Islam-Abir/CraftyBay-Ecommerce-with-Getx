40493933792702d188ec1cb5884efa0afae38de3
Abirul Islam Abir <abir80503@gmail.com>
2023-10-16 15:05:33 +0600

    Update splash_logo.dart

--- lib/modules/widgets/splash_logo.dart
+++ lib/modules/widgets/splash_logo.dart
@@ -1,1 +1,14 @@
-import 'package:flutter/material.dart';import 'package:flutter_svg/svg.dart';import '../../utils/image_assets.dart';class SplashLogo extends StatelessWidget {  const SplashLogo({super.key});  @override  Widget build(BuildContext context) {    return Center(      child: SvgPicture.asset(ImageAsset.primaryLogoSvg, fit: BoxFit.cover),    );  }}
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import '../../utils/image_assets.dart';
+
+class SplashLogo extends StatelessWidget {
+  const SplashLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SvgPicture.asset(ImageAsset.primaryLogoSvg, fit: BoxFit.cover),
+    );
+  }
+}
