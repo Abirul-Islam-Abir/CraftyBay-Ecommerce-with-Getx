2bd7a8927483413a841245ff49954bb04572949e
Abirul Islam Abir <abir80503@gmail.com>
2023-10-16 15:05:07 +0600

    Update mini_heart_button.dart

--- lib/modules/widgets/mini_heart_button.dart
+++ lib/modules/widgets/mini_heart_button.dart
@@ -1,1 +1,26 @@
-import '../../theme/app_colors.dart';import '../../utils/export.dart';class MiniHeartButton extends StatelessWidget {  const MiniHeartButton({    super.key,    required this.isFavPress,    required this.icon,  });  final Function()? isFavPress;  final IconData icon;  @override  Widget build(BuildContext context) {    return InkWell(      onTap: isFavPress,      child: Container(        height: 25,        width: 25,        decoration: BoxDecoration(            borderRadius: BorderRadius.circular(3),            color: AppColor.kPrimaryColor.shade200),        child: Icon(icon, color: AppColor.kRedColor, size: kTextSize),      ),    );  }}
+import '../../theme/app_colors.dart';
+import '../../utils/export.dart';
+
+class MiniHeartButton extends StatelessWidget {
+  const MiniHeartButton({
+    super.key,
+    required this.isFavPress,
+    required this.icon,
+  });
+  final Function()? isFavPress;
+  final IconData icon;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: isFavPress,
+      child: Container(
+        height: 25,
+        width: 25,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(3),
+            color: AppColor.kPrimaryColor.shade200),
+        child: Icon(icon, color: AppColor.kRedColor, size: kTextSize),
+      ),
+    );
+  }
+}
