8f4b714f5d823f794096ab2ee36b7f654e8102ee
Abirul Islam Abir <abir80503@gmail.com>
2023-10-16 14:59:05 +0600

    Update build_carousel_option.dart

--- lib/modules/view screens/home screen/component/build_carousel_option.dart
+++ lib/modules/view screens/home screen/component/build_carousel_option.dart
@@ -1,1 +1,21 @@
-import 'package:carousel_slider/carousel_options.dart';import 'package:flutter/material.dart';CarouselOptions buildCarouselOptions({initialPage, onPageChanged}) {  return CarouselOptions(    height: 160,    aspectRatio: 16 / 9,    viewportFraction: 0.8,    initialPage: initialPage,    enableInfiniteScroll: true,    reverse: false,    autoPlay: true,    autoPlayInterval: const Duration(seconds: 3),    autoPlayAnimationDuration: const Duration(milliseconds: 800),    autoPlayCurve: Curves.fastOutSlowIn,    enlargeCenterPage: true,    enlargeFactor: 0.3,    onPageChanged: onPageChanged,    scrollDirection: Axis.horizontal,  );}
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:flutter/material.dart';
+
+CarouselOptions buildCarouselOptions({initialPage, onPageChanged}) {
+  return CarouselOptions(
+    height: 160,
+    aspectRatio: 16 / 9,
+    viewportFraction: 0.8,
+    initialPage: initialPage,
+    enableInfiniteScroll: true,
+    reverse: false,
+    autoPlay: true,
+    autoPlayInterval: const Duration(seconds: 3),
+    autoPlayAnimationDuration: const Duration(milliseconds: 800),
+    autoPlayCurve: Curves.fastOutSlowIn,
+    enlargeCenterPage: true,
+    enlargeFactor: 0.3,
+    onPageChanged: onPageChanged,
+    scrollDirection: Axis.horizontal,
+  );
+}
