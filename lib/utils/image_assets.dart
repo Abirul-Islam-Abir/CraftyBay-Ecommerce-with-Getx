a8b9917b10a2598809c9703cf0b49011271bef52
Abirul Islam Abir <abir80503@gmail.com>
2023-10-16 15:51:08 +0600

    Update image_assets.dart

--- lib/utils/image_assets.dart
+++ lib/utils/image_assets.dart
@@ -1,1 +1,16 @@
-class ImageAsset {  static String baseImage = 'assets/images';  static String baseIcon = 'assets/icons';  static String primaryLogoSvg = '$baseIcon/logo.svg';  static String diamondSvg = '$baseIcon/diamond.svg';  static String monitorSvg = '$baseIcon/monitor.svg';  static String saladSvg = '$baseIcon/salad.svg';  static String sofaSvg = '$baseIcon/sofa.svg';  static String titleLogoSvg = '$baseIcon/logo_nav.svg';  static String shoes = '$baseImage/shoes.png';  static String noImageNet =      'https://cdn1.iconfinder.com/data/icons/interaction-11/80/picture__gallery__image__error__warning-512.png';  static String profileImageNet =      'https://avatars.githubusercontent.com/u/89778681?s=400&u=81108fd5738acda994cd9504842d1a249f9ae669&v=4';  ImageAsset._();}
+class ImageAsset {
+  static String baseImage = 'assets/images';
+  static String baseIcon = 'assets/icons';
+  static String primaryLogoSvg = '$baseIcon/logo.svg';
+  static String diamondSvg = '$baseIcon/diamond.svg';
+  static String monitorSvg = '$baseIcon/monitor.svg';
+  static String saladSvg = '$baseIcon/salad.svg';
+  static String sofaSvg = '$baseIcon/sofa.svg';
+  static String titleLogoSvg = '$baseIcon/logo_nav.svg';
+  static String shoes = '$baseImage/shoes.png';
+  static String noImageNet =
+      'https://cdn1.iconfinder.com/data/icons/interaction-11/80/picture__gallery__image__error__warning-512.png';
+  static String profileImageNet =
+      'https://avatars.githubusercontent.com/u/89778681?s=400&u=81108fd5738acda994cd9504842d1a249f9ae669&v=4';
+  ImageAsset._();
+}
