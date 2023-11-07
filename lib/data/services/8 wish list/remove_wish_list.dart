085b049e8bf31f308a9ed38aefadc91c897adc47
Abirul Islam Abir <abir80503@gmail.com>
2023-11-08 01:14:39 +0600

    create wish list modified

---
+++ lib/data/services/8 wish list/remove_wish_list.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../url_services.dart';
+
+Future postRemoveWishList(id) async {
+  try {
+    final url = Uri.parse('${APIServices.removeWishListUrl}/$id');
+    final http.Response response =
+    await http.get(url, headers: headerWithTokens);
+    final  responseBody = jsonDecode(response.body);
+    if (response.statusCode == 200 && responseBody['msg'] == 'success') {
+      return responseBody;
+    } else {
+      return responseBody;
+    }
+  } catch (e) {
+    throw Exception('Error has occurred: $e');
+  }
+}
